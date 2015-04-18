ad6bd991cca3ac3e4af155a689ef29e860d5227e
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-04-18 17:38:43 -0400

    spread out code in application.js, add comments & todo comments

--- public/js/application.js
+++ public/js/application.js
@@ -1,30 +1,50 @@
 $(document).ready(function() {
   // TODO:
   // Edit jquery to account for bootstrap modals
-  //  - Something wrong with ajax with this jquery link
+  // - Edit welcome partial
+  //    - Put Directions in Welcome Partial
+  // - When submitting Start, remove directions but
+  //    keep welcome message to user
 
 
   $('#logout').click(function(event) {
+    
     event.preventDefault();
+    
     $link = $(event.target);
+    
     $.ajax({
+    
       url: $link.attr('href'),
+    
       type: 'GET',
+    
     })
+    
     .done(function(welcomePartical) {
+    
       $('header').replaceWith(welcomePartical);
+    
     });
 
   });
 
   $('#start').click(function(event){
+    
     event.preventDefault();
+    
     $link = $(event.target);
+    
     $.ajax({
+    
       url: $link.attr('href'),
+    
       type: 'GET',
+    
     })
+    
     .done(function(gameBoardPartial){
+      
       $('#directions').replaceWith(gameBoardPartial);
 
       // Initialize the Game with a new player 
@@ -38,46 +58,81 @@ $(document).ready(function() {
         // Create Left/Right controls for user 
 
         ['left', 'right'].forEach(function(direction){
+        
           Mousetrap.bind(direction, function() {
+        
             game.player.setDirection(direction)
+        
           });
+        
         });
+        
         setInterval(function() {game.loop(); }, 20);
+    
     });
+  
   });
 
   $('#login-form').submit(function(event) {
     event.preventDefault();
+    
     $form = $(event.target);
+    
     $.ajax({
+      
       url: $form.attr('action'),
+      
       type: $form.attr('method'),
+      
       data: $form.serialize(),
     })
     .done(function(welcomePartical) {
+
+      // Hide signup/login modals, display directions
+      
       $('#login, #signup').modal('hide');
+      
       $('#game-title').toggle();
+      
       $('#directions').show()
 
     })
+    
     .fail(function(){
-      console.log("Failed to login")
+    
+      console.log("Failed to login");
+    
     })
   });
 
   $('#signup-form').submit(function(event) {
+    
     event.preventDefault();
+    
     $form = $(event.target);
+    
     $.ajax({
+    
       url: $form.attr('action'),
+    
       type: $form.attr('method'),
+    
       data: $form.serialize(),
+    
     })
+    
     .done(function(welcomePartical) {
+    
+    // Hide signup/login modals, Show directions
+
       $('#login, #signup').modal('hide');
+    
       $('#game-title').toggle();
+    
       $('#directions').show()
+    
     });
+  
   });
 
 });
